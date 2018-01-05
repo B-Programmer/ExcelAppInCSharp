c9b0ef3bbe42bdafc0f84bc0be9c3c50dd372cc9
B. Programmer <taiwo.balogun@longbridgetech.com>
2018-01-05 15:36:07 +0100

    Code Writing for Excel Console App

---
+++ ExcelAppInCSharp/ExcelConsole.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;       //microsoft Excel 12.0 object in references-> COM tab
+using System.Data;
+//using System.Drawing;
+
+namespace ExcelAppInCSharp
+{
+    public class ExcelConsole
+    {
+        //Create COM Objects. Create a COM object for everything that is referenced
+        Excel.Application xlApp;
+        Excel.Workbook xlWorkbook;
+        Excel._Worksheet xlWorksheet;
+        Excel.Range xlRange;
+
+        public static void getExcelFile()
+        {
+
+            //Create COM Objects. Create a COM object for everything that is referenced
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"C:\B Programmer\C#\ExcelAppInCSharp\the excel wrk bk.xlsx");
+            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+            Excel.Range xlRange = xlWorksheet.UsedRange;
+
+            int rowCount = xlRange.Rows.Count;
+            int colCount = xlRange.Columns.Count;
+
+            //iterate over the rows and columns and print to the console as it appears in the file
+            //excel is not zero based!!
+            for (int i = 1; i <= rowCount; i++)
+            {
+                for (int j = 1; j <= colCount; j++)
+                {
+                    //new line
+                    if (j == 1)
+                        Console.Write("\r\n");
+
+                    //write the value to the console
+                    if (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Value2 != null)
+                        Console.Write(xlRange.Cells[i, j].Value2.ToString() + "\t");
+                }
+            }
+
+            doCleanUp(xlApp, xlWorkbook, (Excel.Worksheet)xlWorksheet, xlRange);
+        }
+
+        
+        public static DataTable ExportToExcel()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("ID", typeof(int));
+            table.Columns.Add("Name", typeof(string));
+            table.Columns.Add("Sex", typeof(string));
+            table.Columns.Add("Subject1", typeof(int));
+            table.Columns.Add("Subject2", typeof(int));
+            table.Columns.Add("Subject3", typeof(int));
+            table.Columns.Add("Subject4", typeof(int));
+            table.Columns.Add("Subject5", typeof(int));
+            table.Columns.Add("Subject6", typeof(int));
+            table.Rows.Add(1, "Amar", "M", 78, 59, 72, 95, 83, 77);
+            table.Rows.Add(2, "Mohit", "M", 76, 65, 85, 87, 72, 90);
+            table.Rows.Add(3, "Garima", "F", 77, 73, 83, 64, 86, 63);
+            table.Rows.Add(4, "jyoti", "F", 55, 77, 85, 69, 70, 86);
+            table.Rows.Add(5, "Avinash", "M", 87, 73, 69, 75, 67, 81);
+            table.Rows.Add(6, "Devesh", "M", 92, 87, 78, 73, 75, 72);
+            return table;
+        }
+
+        public static void CreateExcelFile()
+        {
+            Excel.Application excel;
+            Excel.Workbook worKbooK;
+            Excel.Worksheet worKsheeT;
+            Excel.Range celLrangE;
+
+            try
+            {
+                excel = new Microsoft.Office.Interop.Excel.Application();
+                excel.Visible = true;
+                excel.DisplayAlerts = true;
+                worKbooK = excel.Workbooks.Add(Type.Missing);
+
+
+                worKsheeT = (Microsoft.Office.Interop.Excel.Worksheet)worKbooK.ActiveSheet;
+                worKsheeT.Name = "StudentReportCard";
+
+                worKsheeT.Range[worKsheeT.Cells[1, 1], worKsheeT.Cells[1, 8]].Merge();
+                worKsheeT.Cells[1, 1] = "Student Report Card";
+                worKsheeT.Cells.Font.Size = 15;
+
+
+                int rowcount = 2;
+
+                foreach (DataRow datarow in ExportToExcel().Rows)
+                {
+                    rowcount += 1;
+                    for (int i = 1; i <= ExportToExcel().Columns.Count; i++)
+                    {
+
+                        if (rowcount == 3)
+                        {
+                            worKsheeT.Cells[2, i] = ExportToExcel().Columns[i - 1].ColumnName;
+                            //worKsheeT.Cells.Font.Color = Color.Black;
+
+                        }
+
+                        worKsheeT.Cells[rowcount, i] = datarow[i - 1].ToString();
+
+                        if (rowcount > 3)
+                        {
+                            if (i == ExportToExcel().Columns.Count)
+                            {
+                                if (rowcount % 2 == 0)
+                                {
+                                    celLrangE = worKsheeT.Range[worKsheeT.Cells[rowcount, 1], worKsheeT.Cells[rowcount, ExportToExcel().Columns.Count]];
+                                }
+
+                            }
+                        }
+
+                    }
+
+                }
+
+                celLrangE = worKsheeT.Range[worKsheeT.Cells[1, 1], worKsheeT.Cells[rowcount, ExportToExcel().Columns.Count]];
+                celLrangE.EntireColumn.AutoFit();
+                Excel.Borders border = celLrangE.Borders;
+                border.LineStyle = Microsoft.Office.Interop.Excel.XlLineStyle.xlContinuous;
+                border.Weight = 2d;
+
+                celLrangE = worKsheeT.Range[worKsheeT.Cells[1, 1], worKsheeT.Cells[2, ExportToExcel().Columns.Count]];
+
+                //worKbooK.SaveAs(textBox1.Text);
+                worKbooK.SaveAs("StudentReportCard1");
+                doCleanUp(excel, worKbooK, worKsheeT, celLrangE);
+
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message);
+                Console.Write(ex.Message);
+
+            }
+            finally
+            {
+                worKsheeT = null;
+                celLrangE = null;
+                worKbooK = null;
+            }  
+        }
+
+        public static void doCleanUp(Excel.Application excel,
+            Excel.Workbook xlWorkbook,
+            Excel.Worksheet xlWorkSheet,
+            Excel.Range xlCellRange)
+        {
+            //cleanup
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            //rule of thumb for releasing com objects:
+            //  never use two dots, all COM objects must be referenced and released individually
+            //  ex: [somthing].[something].[something] is bad
+
+            //release com objects to fully kill excel process from running in the background
+            Marshal.ReleaseComObject(xlCellRange);
+            Marshal.ReleaseComObject(xlWorkSheet);
+
+            //close and release
+            xlWorkbook.Close();
+            Marshal.ReleaseComObject(xlWorkbook);
+
+            //quit and release
+            excel.Quit();
+            Marshal.ReleaseComObject(excel);
+        }
+
+    }
+}
